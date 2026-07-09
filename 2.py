dc8a931d619dac1eb9acf1e71a2f35a128d40350
reena47124 <reenarathode47@gmail.com>
2026-07-09 16:25:14 +0530

    update

---
+++ 2.py
@@ -0,0 +1,13 @@
+#leetcode
+#problem 12),convert integer to roman.
+def int_to_roman(num):
+    values=[1000,900,500,400,100,90,50,40,10,9,5,4,1]
+    symbols=["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+    result=""
+    for i in range(len(values)):
+        while num>=values[i]:
+            result+=symbols[i]
+            num-=values[i]
+    return result
+print(int_to_roman(1994))
+print(int_to_roman(4))        
